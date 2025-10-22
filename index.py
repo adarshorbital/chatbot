2dcd8833d718ab4300dda2a02edfc43f78668c62
Adarsh Agrawal <agraw156@purdue.edu>
2025-10-22 16:32:51 -0400

    ADD: second commit to fix

--- index.py
+++ index.py
@@ -7,10 +7,7 @@ import os
 sys.path.insert(0, '/web/groups/gustyai/public_html')
 os.chdir('/web/groups/gustyai/public_html')
 
-# Import your Flask app
-from genai_studio_app import app
+from genaiStudio_app import app
+from wsgiref.handlers import CGIHandler
 
-# Run as CGI
-if __name__ == '__main__':
-    from wsgiref.handlers import CGIHandler
-    CGIHandler().run(app)
+CGIHandler().run(app)
